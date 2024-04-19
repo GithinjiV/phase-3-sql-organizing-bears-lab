54a8b4a7f2e22e618328a5f3badf1f8e3f0b878f
vinnie <githinjigicheru146@gmail.com>
2024-04-19 14:26:35 +0300

    Add bears table and queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 4, "M", "Chocolate", "Happy", true),
+("Rowdy", 4, "M", "Chocolate", "Happy", true),
+("Tabitha", 4, "F", "Chocolate", "Happy", true),
+("Sergeant Brown", 4, "M", "Brown", "Happy", true),
+("Melissa", 4, "F", "White", "Happy", true),
+("Grinch", 4, "M", "Black", "Happy", true),
+("Wendy", 4, "F", "Chocolate", "Happy", true);
